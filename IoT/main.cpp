104699ec64c68467e62ff906d9342ef6b2955dfe
XetzCrd <112039434+XetzCrd@users.noreply.github.com>
2022-09-26 07:42:14 -0500

    Add files via upload

---
+++ IoT/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+void GetHTTPRequest(String url);
+
+const char* ssid[2] = {"redpucp", "Casa"};
+const char* password[2] = {"C9AA28BA93", "SOPADEPOLLOCONCARNE789"};
+int myNet = 1;
+String url = "https://www.google.com/";
+
+void setup() {  
+  Serial.begin(115200);
+  WiFi.begin(ssid[myNet], password[myNet]);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000); Serial.print(".");
+  }
+  	
+  Serial.println("\n Connected to the WiFi network");  
+}
+  
+void loop() {  
+  GetHTTPRequest(url); 
+  while(1);
+}
+
+void GetHTTPRequest(String url){
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+    HTTPClient http;
+   	
+    http.begin(url); //Specify the URL 
+    int httpCode = http.GET();//Make the request
+    
+    if (httpCode > 0) { //Check for the returning code
+        String payload = http.getString();
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+  
+    else {
+      Serial.println("Error on HTTP request");
+    }
+
+    http.end(); //Free the resources
+  }
+}
